2b7bf575be4331bd7d1deb4a06049af086214bee
Andrew Diggs <andrewrdiggs@gmail.com>
2024-07-16 11:39:45 -0500

    worked on some of the game Logic.

--- Yahtzee/GameLogic.cpp
+++ Yahtzee/GameLogic.cpp
@@ -1,4 +1,30 @@
 #include "GameLogic.h"
+#include "Die.h"
 #include <iostream>
 using namespace std;
 
+GameLogic::GameLogic()
+{
+	die1.roll();
+	die2.roll();
+	die3.roll();
+	die4.roll();
+	die5.roll();
+
+};
+void GameLogic::roll()
+{
+	
+};
+int GameLogic::getRoll()
+{
+	
+};
+void GameLogic::addScore()
+{
+
+};
+void GameLogic::keepDie()
+{
+
+};
