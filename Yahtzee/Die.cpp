62e0fa330f57e8157fbb17c13a03ad222d8ddadd
Andrew Diggs <andrewrdiggs@gmail.com>
2024-07-17 13:53:22 -0500

    finished die.cpp

--- Yahtzee/Die.cpp
+++ Yahtzee/Die.cpp
@@ -1,5 +1,23 @@
 #include "Die.h"
 #include <iostream>
 #include <string>
+#include <ctime>
 using namespace std;
 
+Die::Die(int sides) : 
+    sides(sides), faceValue(1) 
+{
+    srand(time(0));  
+    
+    // Initialize random seed
+}
+
+void Die::roll() 
+{
+    faceValue = rand() % sides + 1;
+}
+
+int Die::getFaceValue() const 
+{
+    return faceValue;
+}
